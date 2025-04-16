30f2b9e714b7d454b1b9a1afe2d5a74def03e5a6
Denis Matsenko <matsenkodenis0@gmail.com>
2025-04-16 10:43:39 +0200

    udpate

--- app/components/scene.tsx
+++ app/components/scene.tsx
@@ -6,19 +6,28 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 const SpinningCube = () => {
-  const meshRef = useRef<THREE.Mesh>(null!);
+  const cubeMeshRef = useRef<THREE.Mesh>(null!);
 
   useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
+    if (cubeMeshRef.current) {
+      cubeMeshRef.current.rotation.x += 0.01;
+      cubeMeshRef.current.rotation.y += 0.01;
     }
   });
 
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
+    <mesh ref={cubeMeshRef}>
+      <boxGeometry args={[0.5, 0.5, 0.5]} />
+      <meshStandardMaterial color="green" />
+    </mesh>
+  );
+};
+
+const Sphere = () => {
+  return (
+    <mesh position={[1, 1, 0]}>
+      <sphereGeometry args={[0.2, 32, 32]} />
+      <meshStandardMaterial color="red" />
     </mesh>
   );
 };
@@ -26,10 +35,11 @@ const SpinningCube = () => {
 const Scene = () => {
   return (
     <Canvas style={{ height: "100vh", width: "100vw" }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
+      {/* <ambientLight intensity={0.5} /> */}
+      {/* <pointLight position={[30, 30, 30]} /> */}
       <SpinningCube />
-      <OrbitControls />
+      <Sphere />
+      {/* <OrbitControls /> */}
     </Canvas>
   );
 };
